1951e4997df6dda41888cbc803b45b2365fd3aa1
Arpan Basak <arpanbasak90@gmail.com>
2026-07-30 19:04:57 +0530

    fix: set turbopack root in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: { unoptimized: true },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 module.exports = nextConfig;
